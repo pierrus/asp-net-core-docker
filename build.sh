7043fb3c049dab6053ba5acab3a7dc3fe021d384
Pierrus <pierre@c17e.com>
2019-10-09 11:16:49 +0200

    Conversion projet test + MAJ scripts de build

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-rm -r build
-rm -r docker/build
-dotnet publish src/todo -o ./build -f netcoreapp1.1
+[ -d "build" ] && rm -r build
+[ -d "docker/build" ] && rm -r docker/build
+[ -d "src/todo/build" ] && rm -r src/todo/build
+
+dotnet publish src/todo -o build -f netcoreapp1.1
 cp -a src/todo/build/. docker/build
